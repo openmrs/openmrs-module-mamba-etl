beb42f68b83a589544f2395a90682848c81cc9ff
laureen <laureen.omare@ucgloballprograms.org>
2023-11-16 18:29:45 +0300

    Synchronizing the commits

--- omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
+++ omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
@@ -8,4 +8,13 @@ CALL sp_mamba_data_processing_flatten();
 CALL sp_mamba_data_processing_derived_hts();
 -- CALL sp_mamba_data_processing_derived_covid();
 CALL sp_mamba_data_processing_derived_pmtct();
+
+-- TODO: Automate calling of these SPs that create Column Tables
+-- TODO: Can use this but think through first: CALL sp_mamba_generate_report_wrapper(1, 'total_deliveries', '');
+-- TODO: Or use the option of apache datasource that can get column names needs testing
+CALL sp_mamba_total_pregnant_women_columns_query;
+Call sp_mamba_hiv_exposed_infants_columns_query;
+Call sp_mamba_total_deliveries_columns_query;
+CALL sp_mamba_mother_hiv_status_query('', '');
+
 -- $END
