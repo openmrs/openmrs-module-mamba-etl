b9cad4de01896bd6ec421f05050866ab61398d98
smallgod <davies.mugume@gmail.com>
2024-07-22 15:47:16 +0300

    testing incremental updates

--- api/src/main/resources/mamba/mamba_etl_scheduler.sql
+++ api/src/main/resources/mamba/mamba_etl_scheduler.sql
@@ -1,30 +1,10 @@
+-- Enable the event scheduler
 SET GLOBAL event_scheduler = ON;
 
-CREATE EVENT IF NOT EXISTS events_mamba_etl
-    ON SCHEDULE EVERY ? MINUTE
+-- Create the event to call the stored procedure
+DROP EVENT IF EXISTS _mamba_etl_scheduler_event;
+
+CREATE EVENT IF NOT EXISTS _mamba_etl_scheduler_event
+    ON SCHEDULE EVERY 1 MINUTE
         STARTS CURRENT_TIMESTAMP
-    DO
-    BEGIN
-
-        DECLARE etl_schedule_table_count INT DEFAULT 1;
-        DECLARE incremental_mode TINYINT(1) DEFAULT 1;
-
-        -- TODO: make etl table name dynamic
-        SELECT COUNT(*)
-        INTO etl_schedule_table_count
-        FROM information_schema.tables
-        WHERE table_schema = 'analysis_db'
-          AND table_name = '_mamba_etl_schedule';
-
-        SELECT DISTINCT(incremental_mode_switch)
-        INTO incremental_mode
-        FROM mamba_etl_user_settings;
-
-        IF etl_schedule_table_count < 1 OR incremental_mode = 0 THEN
-            CALL sp_mamba_etl_schedule_table_create();
-            CALL sp_mamba_etl_schedule('sp_mamba_data_processing_flatten');
-        ELSE
-            CALL sp_mamba_etl_schedule('sp_mamba_data_processing_flatten_incremental');
-        END IF;
-
-    END;
+    DO CALL sp_mamba_etl_scheduler_wrapper();
