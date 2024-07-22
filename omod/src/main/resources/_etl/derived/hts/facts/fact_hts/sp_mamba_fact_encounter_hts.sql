175c3432eef021fabb0244c3b9c8640226b035a4
Arthur D. Mugume <davies.mugume@gmail.com>
2024-07-23 02:29:42 +0300

    Resolves issues with reporting (#51)
    * updates with latest feature support
    
    * testing incremental updates
    
    * resolve issue with reports on working branch
    
    * support reports
    
    * remove unwanted commented out code

--- omod/src/main/resources/_etl/derived/hts/facts/fact_hts/sp_mamba_fact_encounter_hts.sql
+++ omod/src/main/resources/_etl/derived/hts/facts/fact_hts/sp_mamba_fact_encounter_hts.sql
@@ -1,5 +1,5 @@
 -- $BEGIN
 CALL sp_mamba_fact_encounter_hts_create();
-CALL sp_mamba_fact_encounter_hts_insert();
+-- CALL sp_mamba_fact_encounter_hts_insert();
 CALL sp_mamba_fact_encounter_hts_update();
 -- $END
