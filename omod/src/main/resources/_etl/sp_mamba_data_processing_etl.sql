88d0867e59b01e2e344764def6f81ac95d105e7c
laureen <laureen.omare@ucgloballprograms.org>
2024-08-13 13:34:00 +0300

    Remove call to drop and flatten

--- omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
+++ omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
@@ -1,9 +1,6 @@
 -- $BEGIN
 -- add base folder SP here --
 
--- Flatten the tables first
-CALL sp_mamba_data_processing_drop_and_flatten();
-
 -- Call the ETL process
 CALL sp_mamba_data_processing_derived_hts();
 CALL sp_mamba_data_processing_derived_pmtct();
