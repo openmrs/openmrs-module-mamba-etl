f8d42ba6659d13371b92dfa7f1de39102b7b58be
Arthur D. Mugume <davies.mugume@gmail.com>
2024-08-16 13:27:09 +0200

    Fix missing concepts and artifacts and  (#61)
    * resolved issues with missing concepts and uuids
    
    * modified sp_mamba_data_processing_etl to accept incremental param

--- omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
+++ omod/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
@@ -1,9 +1,19 @@
--- $BEGIN
--- add base folder SP here --
+DELIMITER //
 
--- Call the ETL process
-CALL sp_mamba_data_processing_derived_hts();
-CALL sp_mamba_data_processing_derived_pmtct();
+DROP PROCEDURE IF EXISTS sp_mamba_data_processing_etl;
+
+CREATE PROCEDURE sp_mamba_data_processing_etl(IN etl_incremental_mode INT)
+
+BEGIN
+    -- add base folder SP here if any --
+
+    -- Needed for now till incremental is full implemented. We will just drop all tables and recreate them
+    -- CALL sp_mamba_drop_all_billing_tables();
+    -- Call the ETL process
+    CALL sp_mamba_data_processing_derived_hts();
+    CALL sp_mamba_data_processing_derived_pmtct();
 -- CALL sp_mamba_data_processing_derived_covid();
 
--- $END
+END //
+
+DELIMITER ;
