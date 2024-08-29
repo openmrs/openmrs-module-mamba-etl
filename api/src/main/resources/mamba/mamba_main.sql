fa08b922412d7226808c21ad33e11e13af088d46
laureen <laureen.omare@ucgloballprograms.org>
2024-08-29 09:25:36 +0300

    Trim records in the scheduker table

--- api/src/main/resources/mamba/mamba_main.sql
+++ api/src/main/resources/mamba/mamba_main.sql
@@ -12,3 +12,20 @@ CREATE EVENT IF NOT EXISTS _mamba_etl_scheduler_event
     ON SCHEDULE EVERY ? SECOND
         STARTS CURRENT_TIMESTAMP
     DO CALL sp_mamba_etl_schedule();
+
+-- Setup a trigger that trims record off _mamba_etl_schedule to just leave 20 latest records.
+       -- to avoid the table growing too big
+CREATE TRIGGER trim_log_table_after_insert
+    AFTER INSERT ON _mamba_etl_schedule
+    FOR EACH ROW
+BEGIN
+    DELETE FROM _mamba_etl_schedule
+    WHERE id NOT IN (
+        SELECT id FROM (
+                           SELECT id
+                           FROM _mamba_etl_schedule
+                           ORDER BY id DESC
+                               LIMIT 20
+                       ) AS recent_records
+    );
+END;
